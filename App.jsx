40ad632bdb319eafbaf05689eecd0c99f753cfb8
pullannagarilaxmikanthreddy114 <pullannagarilaxmikanthreddy114@gmail.com>
2026-06-12 11:31:12 +0530

    week10

--- App.jsx
+++ App.jsx
@@ -1,18 +1,66 @@
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-import Home from "./components/Home";
-import AddBook from "./components/AddBook";
+import { useState } from "react";
+
+import {
+ BrowserRouter,
+ Routes,
+ Route
+}
+from "react-router-dom";
+
+import Login from "./components/Login";
+import Dashboard from "./components/Dashboard";
+import BookDeletion from "./components/BookDeletion";
+import ProtectedRoute from "./routes/ProtectedRoute";
+
 function App() {
+
+  const [isLoggedIn,
+         setIsLoggedIn] =
+         useState(false);
+
   return (
+
     <BrowserRouter>
-      <nav>
-        <Link to="/">Home</Link> |
-        <Link to="/addbook"> Add Book</Link>
-      </nav>
+
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/addbook" element={<AddBook />} />
+
+        <Route
+          path="/"
+          element={
+            <Login
+              setIsLoggedIn={
+                setIsLoggedIn
+              }
+            />
+          }
+        />
+
+        <Route
+          path="/dashboard"
+          element={
+            <ProtectedRoute
+              isLoggedIn={isLoggedIn}
+            >
+              <Dashboard />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/deletebook"
+          element={
+            <ProtectedRoute
+              isLoggedIn={isLoggedIn}
+            >
+              <BookDeletion />
+            </ProtectedRoute>
+          }
+        />
+
       </Routes>
+
     </BrowserRouter>
+
   );
 }
 
