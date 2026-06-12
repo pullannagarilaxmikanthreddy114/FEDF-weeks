594956f571e876dbd86dffd35110c1cac9367a8c
pullannagarilaxmikanthreddy114 <pullannagarilaxmikanthreddy114@gmail.com>
2026-06-12 11:30:42 +0530

    week9

--- App.jsx
+++ App.jsx
@@ -1,12 +1,19 @@
-import Weather from "./components/Weather";
-
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Home from "./components/Home";
+import AddBook from "./components/AddBook";
 function App() {
   return (
-    <div>
-      <Weather />
-    </div>
+    <BrowserRouter>
+      <nav>
+        <Link to="/">Home</Link> |
+        <Link to="/addbook"> Add Book</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/addbook" element={<AddBook />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
 export default App;
-
