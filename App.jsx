22eae3f11b527a7b93f049346ae02a2143cf7b39
pullannagarilaxmikanthreddy114 <pullannagarilaxmikanthreddy114@gmail.com>
2026-06-12 11:30:13 +0530

    week8

--- App.jsx
+++ App.jsx
@@ -1,25 +1,12 @@
-import { useState } from "react";
-import StudentContext from "./StudentContext";
-import StudentForm from "./components/StudentForm";
-import StudentList from "./components/StudentList";
+import Weather from "./components/Weather";
 
 function App() {
-
-    // State stored in Parent Component
-    const [students, setStudents] = useState([]);
-
-    return (
-        <StudentContext.Provider
-            value={{ students, setStudents }}
-        >
-            <div>
-                <h1>Student Management System</h1>
-
-                <StudentForm />
-                <StudentList />
-            </div>
-        </StudentContext.Provider>
-    );
+  return (
+    <div>
+      <Weather />
+    </div>
+  );
 }
 
 export default App;
+
